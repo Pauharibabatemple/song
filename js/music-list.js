fa21bbf110db86afe49f1eeff21d9d0b9c590b0f
Pauhari Baba <95703538+Pauharibabatemple@users.noreply.github.com>
2022-07-31 16:55:46 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,82 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Hawa Banke",
+    artist: "Darshan Raval",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Jannat Ve",
+    artist: "Darshan Raval",
+   img:"music-2",
+    src: "music-2"
+  },
+  {
+    name: "Zara Sa",
+    artist: "KK and Pritam",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Bhool Bhulaiyaa",
+    artist: "Unknown",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Shayad - love Aaj Kal",
+    artist: "Arijit Singh",
+    img: "music-5",
+    src: "music-5"
+  },
+  
+  {
+    name: "Chhod Diya ",
+    artist: "Arijit Singh",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Jo bejhi thi dua ",
+    artist: "Arijit Singh",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Baraf ",
+    artist: "Khesari Lal Yadav, Neha Raj",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Ta Aehi Yehi Khatir Ara Aaile ",
+    artist: "Pawan Singh",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Nathuniya ",
+    artist: "Khesari Lal Yadav and Priyanka Singh",
+    img: "music-10",
+    src: "music-10"
+  },
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
